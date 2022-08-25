65209c71cf94f2794dd05a3091fd7c74b6d9412e
Abhinay sharma <abhinaysharma127@gmail.com>
2022-08-25 13:03:19 +0530

    u

--- router/menu.js
+++ router/menu.js
@@ -34,5 +34,32 @@ router.post ('/', async (req, res) =>{
         res.status(400).json(err);
     }
 
+    
+
+})
+
+router.put ('/update', async (req, res) =>{
+    try{
+        const tempMenu = {
+            rest_id: req.body.rest_id,
+            food_id: req.body.food_id,
+            food_name: req.body.food_name,
+            food_type: req.body.food_type,
+            food_category: req.body.food_category,
+            price: req.body.price,
+            image: req.body.image,  
+            description: req.body.description
+        }
+        // database
+        // const response = await tempMenu.save();
+        const response = await Menu.findOneAndUpdate({'food_id': tempMenu.food_id}, tempMenu, {new: true});
+        res.status(201).json(response);
+    }
+    catch(err){
+        res.status(400).json(err);
+    }
+
+    
+
 })
 module.exports = router;
